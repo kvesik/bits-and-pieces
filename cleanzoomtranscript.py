40450105d0ac8c3c016c0e35fd6f5443caebfcf9
kvesik <33950744+kvesik@users.noreply.github.com>
2023-09-14 17:03:52 -0700

    Update cleanzoomtranscript.py

--- cleanzoomtranscript.py
+++ cleanzoomtranscript.py
@@ -1,21 +1,26 @@
 import io
 
-def cleantranscript(filepath, removelinebreaks):
+
+def cleantranscript(filepath, removetiminginformation, removeblanklines, removelinebreaks):
     endoflinewhitespace = " " if removelinebreaks else "\n"
-    filesuffix = "_cleaned" + ("_nolinebreaks" if removelinebreaks else "") + ".txt"
+    filesuffix = "_cleaned" + ("_notiming" if removetiminginformation else "") + ("_noblanklines" if removeblanklines else "") + ("_nolinebreaks" if removelinebreaks else "") + ".txt"
     with io.open(filepath, "r", encoding="utf-8") as src:
         with io.open(filepath.replace(".cc.vtt", filesuffix), "w", encoding="utf-8") as dest:
             sline = src.readline()
             while len(sline) > 0:
                 sline = sline.strip()
-                if "WEBVTT" in sline or sline == "" or (":" in sline and "-->" in sline):
-                    # skip this line
-                    pass
-                else:
+                skipthisline = "WEBVTT" in sline
+                if removetiminginformation:
+                    skipthisline = skipthisline or (":" in sline and "-->" in sline)
+                if removeblanklines:
+                    skipthisline = skipthisline or (sline == "")
+                if not skipthisline:
                     dest.write(sline + endoflinewhitespace)
                 sline = src.readline()
             dest.write("\n")
 
 filepath = input("Enter full path for file of type .cc.vtt: ")
-removelinebreaks = input("This utility always removes timing information and blank lines.\nDo you ALSO want to remove the arbitrary line breaks that\nzoom inserts every 15 or so words? Enter either 'y' or 'n'. ").lower()
-cleantranscript(filepath, removelinebreaks)
+removetiminginformation = input("Would you like to remove timing information from each line? Enter 'y' or 'n'. ").lower() == "y"
+removeblanklines = input("Would you like to remove the blank line between each line of text? Enter 'y' or 'n'. ").lower() == "y"
+removelinebreaks = False if (not removeblanklines or not removetiminginformation) else input("Would you like to remove the arbitrary line breaks that zoom inserts every 15 or so words? Enter 'y' or 'n'. ").lower() == "y"
+cleantranscript(filepath, removetiminginformation, removeblanklines, removelinebreaks)
